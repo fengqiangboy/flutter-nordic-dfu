8b8bb2892aa53ecd24e131caa1813d46966ac1f1
fengqiang-mbp <498460828@qq.com>
2019-01-17 16:19:15 +0800

    修改dfuservice

--- android/src/main/kotlin/com/timeyaa/flutternordicdfu/DfuService.kt
+++ android/src/main/kotlin/com/timeyaa/flutternordicdfu/DfuService.kt
@@ -4,9 +4,9 @@ import android.app.Activity
 import no.nordicsemi.android.dfu.DfuBaseService
 
 class DfuService : DfuBaseService() {
-    override fun getNotificationTarget(): Class<out Activity> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
     private val TAG = "DfuService"
+
+    override fun getNotificationTarget(): Class<out Activity> {
+        return NotificationActivity::class.java
+    }
 }
