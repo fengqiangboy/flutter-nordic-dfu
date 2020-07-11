4e194f6f8ee2de6e73ab8403518697fc4083fce6
Brad Egler <begler@gmail.com>
2020-07-11 09:33:09 -0400

    Fixes #14 - Newer versions of the Flutter plugin system have a guard around the generated header file. Without that guard some projects will not build.

--- ios/Classes/FlutterNordicDfuPlugin.m
+++ ios/Classes/FlutterNordicDfuPlugin.m
@@ -1,5 +1,12 @@
 #import "FlutterNordicDfuPlugin.h"
+#if __has_include(<flutter_nordic_dfu/flutter_nordic_dfu-Swift.h>)
 #import <flutter_nordic_dfu/flutter_nordic_dfu-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "flutter_nordic_dfu-Swift.h"
+#endif
 
 @implementation FlutterNordicDfuPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
